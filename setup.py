d64643cadcd5b590d4fa01d0c927612f3063a6fd
Artur Yakimovich <artur.yakimovich@imls.uzh.ch>
2011-10-19 16:45:32 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Artur
+#
+# Created:     18.10.2011
+# Copyright:   (c) Artur 2011
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['cleanthumbs.py'])
